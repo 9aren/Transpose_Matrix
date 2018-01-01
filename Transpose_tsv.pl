7a3c634b7484173ba3c9ab525bd0cf5d5c43c8ac
Narendrakumar Chaudhari <naren.niper@gmail.com>
2018-01-01 15:04:37 +0530

    Update Transpose_tsv.pl

--- Transpose_tsv.pl
+++ Transpose_tsv.pl
@@ -1,21 +1,22 @@
+#!/usr/bin/perl
 #Transposes tab seperated plain text (.tsv) matrix form 
-	open (FILE, "infile.tsv");
-	open (OUT, ">transposed.txt");
-	@all_data=<FILE>;
-	my @temp1=split/\t/,$all_data[0];
-	my $all_count=@temp1; 
-	for my $index(0..$all_count){
-		foreach (@all_data){
-			$_=~s/\r|\n|\r\n//;
-			my @temp2=split/\t/,$_;
-			print OUT $temp2[$index]."\t";
-		}
-		print OUT "\n";
+open (FILE, "infile.tsv"); #must be a TAB seperated file.
+open (OUT, ">transposed.txt");
+@all_data=<FILE>;
+my @temp1=split/\t/,$all_data[0];
+my $all_count=@temp1; 
+for my $index(0..$all_count){
+	foreach (@all_data){
+		$_=~s/\r|\n|\r\n//;
+		my @temp2=split/\t/,$_;
+		print OUT $temp2[$index]."\t";
 	}
-	close FILE;
-	close OUT;
-	undef @temp1;
-	undef @temp2;
-	undef @all_data;
+	print OUT "\n";
+}
+close FILE;
+close OUT;
+undef @temp1;
+undef @temp2;
+undef @all_data;
 
 
